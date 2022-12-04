c2378675cb75c0110c6655f1afb91992893ff098
alexsandrosr88 <alexsandrosr@msn.com>
2022-12-04 20:59:28 -0300

    Concluído Bee1047

--- exercicios/Bee1047.java
+++ exercicios/Bee1047.java
@@ -4,52 +4,74 @@ import java.util.Scanner;
 
 public class Bee1047 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc =  new Scanner(System.in);
 		
-//		int hI = sc.nextInt();
-//		int mI = sc.nextInt();
-//		int hF = sc.nextInt();
-//		int mF = sc.nextInt();
+		int hI = sc.nextInt();
+		int mI = sc.nextInt();
+		int hF = sc.nextInt();
+		int mF = sc.nextInt();
 		
 		sc.close();
 		
-		int hI = 8;
-		int mI = 10;
-		int hF = 10;
-		int mF = 2;
-		
-		
-		int calcHoras, minTotal, horaFim, minFim;	
-		
 		if(hI == hF && mI == mF)
-			System.out.println("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
-		else if(hI > 12 && hF < 12) {
-			calcHoras = 24 - hI + hF;
+			System.out.println("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");		
+		else if(hI > hF && mI<=mF) {
+			int calHoras = (Math.abs((24-hI) + hF))*60;
+			int calMin = Math.abs(mI - mF);
+			int minTotal = Math.abs(calHoras + calMin);
 			
-			minTotal = calcHoras * 60 - (mI + mF);
-			
-			horaFim = minTotal / 60;
-			minFim = minTotal % 60;
-			
-			System.out.println("O JOGO DUROU "+ horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
+			int horaFim = minTotal / 60;
+			int minFim = minTotal % 60;
+			System.out.println("O JOGO DUROU "+horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
 		}
-		else if(hI < 12 && hF > 12) {
-			calcHoras = 12 - hI + hF;
+		else if(hI >= hF && mI>=mF) {
+			int calHoras = (Math.abs((24-hI) + hF))*60;
+			int calMin = Math.abs(mI - mF);
+			int minTotal = Math.abs(calHoras - calMin);
 			
-			minTotal = calcHoras * 60 - (mI + mF);
-			horaFim = minTotal / 60;
-			minFim = minTotal % 60;
+			int horaFim = minTotal / 60;
+			int minFim = minTotal % 60;
+			System.out.println("O JOGO DUROU "+horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
+		}
+		else if(hI <=12 && hF <=12 && mI<mF) {
+			int calHoras = (Math.abs((12-hI) - (12-hF)))*60;
+			int calMin = Math.abs(mI - mF);
+			int minTotal = Math.abs(calHoras + calMin);
 			
-			System.out.println("O JOGO DUROU "+ horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
+			int horaFim = minTotal / 60;
+			int minFim = minTotal % 60;
+			
+			System.out.println("O JOGO DUROU "+horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
+		}
+		else if(hI <=12 && hF <=12 && mI>mF) {
+			int calHoras = (Math.abs((12-hI) - (12-hF)))*60;
+			int calMin = Math.abs(mI - mF);
+			int minTotal = Math.abs(calHoras - calMin);
+			
+			int horaFim = minTotal / 60;
+			int minFim = minTotal % 60;
+			
+			System.out.println("O JOGO DUROU "+horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
+		}
+		else if(hI > 12 && mI<=mF) {
+			int calHoras = (Math.abs(hI - hF))*60;
+			int calMin = Math.abs(mI - mF);
+			int minTotal = Math.abs(calHoras + calMin);
+			
+			int horaFim = minTotal / 60;
+			int minFim = minTotal % 60;
+			
+			System.out.println("O JOGO DUROU "+horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
 		}
 		else {
-			calcHoras = hF - hI;
+			int calHoras = (Math.abs(hI - hF))*60;
+			int calMin = Math.abs(mI - mF);
+			int minTotal = Math.abs(calHoras - calMin);
 			
-			minTotal = calcHoras * 60 + (mI + mF);
-			horaFim = minTotal / 60;
-			minFim = minTotal % 60;
+			int horaFim = minTotal / 60;
+			int minFim = minTotal % 60;
 			
-			System.out.println("O JOGO DUROU "+ horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
+			System.out.println("O JOGO DUROU "+horaFim+" HORA(S) E "+minFim+" MINUTO(S)");
 		}
 	}
 }
