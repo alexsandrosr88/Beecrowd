11fdc25b1260cd36f9c5a50e8d66c1819fee366c
Alexsandro Ramos <63133361+alexsandrosr88@users.noreply.github.com>
2022-12-05 01:05:38 -0300

    Corrigido 1049

--- exercicios/Bee1049.java
+++ exercicios/Bee1049.java
@@ -1,5 +1,77 @@
 package Exercicios;
 
-public class Bee1049 {
+import java.util.Scanner;
 
+public class Bee1049 {
+  Scanner sc = new Scanner(System.in);
+		
+		String palavra1 = sc.nextLine();
+		
+		String palavra2 = sc.nextLine();
+
+		String palavra3 = sc.nextLine();
+		
+		sc.close();
+		
+		switch (palavra1) {
+		case "vertebrado":
+			switch (palavra2) {
+			case "ave":
+				switch (palavra3) {
+				case "carnivoro":
+					System.out.println("aguia");
+					break;
+				case "onivoro":
+					System.out.println("pomba");
+					break;
+				default:
+					break;
+				}	
+				break;
+			case "mamifero":
+				switch (palavra3) {
+				case "onivoro":
+					System.out.println("homem");
+					break;
+				case "herbivoro":
+					System.out.println("vaca");
+				default:
+					break;
+				}
+			default:
+				break;
+			}
+			break;
+		case "invertebrado":
+			switch (palavra2) {
+			case "inseto":
+				switch (palavra3) {
+				case "hematofago":
+					System.out.println("pulga");
+					break;
+				case "herbivoro":
+					System.out.println("lagarta");
+					break;
+				default:
+					break;
+				}
+				break;
+			case "anelideo":
+				switch (palavra3) {
+				case "hematofago":
+					System.out.println("sanguessuga");
+					break;
+				case "onivoro":
+					System.out.println("minhoca");
+					break;
+				default:
+					break;
+				}
+			default:
+				break;
+			}
+		default:
+			break;
+		}		
+	}
 }
