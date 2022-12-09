26b0aef1e61f6f097de1268969f12c47bbe32115
Alexsandro Ramos <63133361+alexsandrosr88@users.noreply.github.com>
2022-12-09 03:41:06 -0300

    Concluído 1079

---
+++ exercicios/Bee1079.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bee1079 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			double c = sc.nextDouble();
+			
+			double media = (a*2+b*3+c*5)/10;
+			
+			System.out.println(String.format("%.1f", media));
+		}
+		sc.close();
+	}
+}
