c515a801da8bc37a81b3e04ebcb7ca3eb744419f
Alexsandro Ramos <63133361+alexsandrosr88@users.noreply.github.com>
2022-12-11 23:40:28 -0300

    Concluído 1096

---
+++ exercicios/Bee1096.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class Bee1096 {
+	public static void main(String[] args) {
+				
+		for(int i=1; i<=9;i++) {			
+			for(int j = 7; j>4;j--) {
+				System.out.print("I="+i+" ");
+				System.out.println("J="+j);
+			}
+			i+=1;
+		}
+	}
+}
