c62e5acdd4a17e0ee56ae7ffec55ea45704379e6
Alexsandro Ramos <63133361+alexsandrosr88@users.noreply.github.com>
2022-12-23 04:14:07 -0300

    Atualizado algoritmo1044

--- exercicios/Bee1044.java
+++ exercicios/Bee1044.java
@@ -5,34 +5,19 @@ import java.util.Scanner;
 public class Bee1044 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		int x = 0;
-		
+
 		sc.close();
-		
-		if(a < b) {
-			for(int i =1; x != b; i++) {
-				x = a *i;
-				if(x > b) {
-					System.out.println("Nao sao Multiplos");
-				break;
-				}
-			}
-			if(x == b)
-				System.out.println("Sao Multiplos");	
-		}
-		else {
-			for(int i =1; x != a; i++) {
-				x = b *i;
-				if(x > a) {
-					System.out.println("Nao sao Multiplos");
-				break;
-				}
-			}
-			if(x == a)
-				System.out.println("Sao Multiplos");
-		}
+
+		if (b > a && b % a == 0)
+			System.out.println("Sao Multiplos");
+		else if (b > a && b % a != 0)
+			System.out.println("Nao sao Multiplos");
+		else if (a > b && a % b == 0)
+			System.out.println("Sao Multiplos");
+		else
+			System.out.println("Nao sao Multiplos");
 	}
 }
